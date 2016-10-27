a606a938c11cda7012b7bbd2a1d805c9e9436580
mbr84 <matthew.bramfeld@gmail.com>
2016-10-26 17:54:49 -0700

    minor styling changes

--- public/modal.js
+++ public/modal.js
@@ -11,7 +11,7 @@ $('#cover').click(() => {
 
 const errorMessage = `<h4 class="error-message">There was an error fulfilling your request. Please ensure you've entered a vaild email address and try again</h4>`;
 const successMessage = `
-<h3 class="success-message">We have received your request. An agent will be in touch with you shortly</h3>`;
+<h3 class="success-message"><strong>Thank You</strong><br><br>We received your request. An agent will be in touch with you shortly</h3>`;
 
 $('.submit').click((event) => {
   event.preventDefault();

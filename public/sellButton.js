d288bc88c5ebe67508d11e2b634c818189864d1f
mbr84 <matthew.bramfeld@gmail.com>
2016-11-06 13:04:45 -0800

    follow

--- public/sellButton.js
+++ public/sellButton.js
@@ -1,24 +1,17 @@
 /* eslint-disable no-undef */
 
 $(document).ready(() => {
-  let elBottom = $('.edit').offset().top + $('.edit').height();
+  let elTop = $('.edit').offset().top;
   const toggleFollowerButton = () => {
-    if (window.innerWidth > 550) {
-      let percentRight = 50 - (50 * ($('.follow').width() / window.innerWidth));
-      percentRight += '%';
-      $('.follow').css({ right: percentRight });
+    if (elTop - $(window).scrollTop() < 0) {
+      $('.follow').css('display', 'block');
     } else {
-      $('.follow').css({ right: '8%' });
-    }
-    if (elBottom - $(window).scrollTop() < -100) {
-      $('.follow').css('top', '1px');
-    } else {
-      $('.follow').css('top', '-60px');
+      $('.follow').css('display', 'hidden');
     }
   };
 
   $(window).resize(() => {
-    elBottom = $('.edit').offset().top;
+    elTop = $('.edit').offset().top;
     toggleFollowerButton();
   });
 
@@ -26,3 +19,19 @@ $(document).ready(() => {
     toggleFollowerButton();
   });
 });
+
+// for dropdown animated follow button
+// const toggleFollowerButton = () => {
+//   if (window.innerWidth > 550) {
+//     let percentRight = 50 - (50 * ($('.follow').width() / window.innerWidth));
+//     percentRight += '%';
+//     $('.follow').css({ right: percentRight });
+//   } else {
+//     $('.follow').css({ right: '8%' });
+//   }
+//   if (elBottom - $(window).scrollTop() < -100) {
+//     $('.follow').css('top', '1px');
+//   } else {
+//     $('.follow').css('top', '-60px');
+//   }
+// };
